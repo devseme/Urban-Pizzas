f6778151afc1e959369d4a1cb017d0e28b71b5be
devseme <ian.ochenge@student.moringaschool.com>
2021-09-19 22:28:15 +0300

    worked on functions

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,23 +1,52 @@
 //business logic
 
-function totalAmount(size,crust,toppings){
+function totalAmount(size,crust,toppings,number){
     this.size = size;
     this.crust = crust;
     this.toppings = toppings;
+    this.number = number;
 
 }
       totalAmount.prototype.Price = function(){
+
+
             
           return this.size + this.crust + this.toppings ;
+
       }
+        totalAmount.prototype.totalForPizza = function(){
+             
+
+            
+          return this.number*(this.size + this.crust + this.toppings) ;
+        }
 
 
 
 
 $(document).ready(function () {
 
-    let pizzaSize = parseInt($("#size").value());
-    let pizzaCrust = parseInt($("#crust").value());
-    let toppings = parseInt($("#toppings").value())
+   
+       
+       $("#amount").click(function(){
+        let pizzaSize = parseInt($("#size").val());
+        let pizzaCrust = parseInt($("#crust").val());
+        let toppings = parseInt($("#toppings").val());
+
+          let total = new totalAmount(pizzaSize,pizzaCrust,toppings)
+            
+
+           $("#total").text(total.Price());
+          let numberOfPizzas = parseInt($ ("#number").val());
+             let amountPaid = new totalAmount(pizzaSize,pizzaCrust,toppings,numberOfPizzas);
+               
+             $("#total").text(" Your total amount to pay is : "  + "ksh" + amountPaid.totalForPizza());
+
+               
+
+          
+                
+          
+       });
 
 });
